732413b67802bc1535fa0b5380865114856dbc10
szare <szarekwiktor13@gmail.com>
2026-09-18 10:40:40 +0200

    ZAD-01 added fallback and retrying with test using wiremock

--- src/main/java/com/example/test/client/GithubClient.java
+++ src/main/java/com/example/test/client/GithubClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "github", url = "https://api.github.com/",
+@FeignClient(name = "github",
         configuration = GithubFeignConfig.class,
         fallbackFactory = GithubClientFallbackFactory.class)
 public interface GithubClient {
